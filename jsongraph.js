cf26986d84cb4a5abb88b2cddc71a28bb04dc2bb
Hubert Grzywacz <hgrzywacz@kbj.com.pl>
2014-08-04 16:23:54 +0200

    Create null nodes and command line arguments

--- jsongraph.js
+++ jsongraph.js
@@ -1,106 +1,116 @@
 #! /usr/bin/env node
 
-var json = require('./b2.json');
+var argv = process.argv;
 
-var edges = [];
-
-var colors = {
-  'object': '#F78181',
-  'array': 'gold2',
-  'number': 'darkseagreen',
-  'string': 'beige'
+if (argv.length > 2) {
+  argv.slice(2).forEach(generateGraph);
 };
 
-var printHead = function () {
-  return 'digraph d {\n' +
-    'rankdir=LR\n' +
-    'nodesep=0.1\n' +
-    'concentrate=true\n' +
-    'splines=true\n' +
-    'node [fontname="Helvetica,Sans", fontsize=9, penwidth=1, margin="0.5, 0.04", height="0.2" ]\n' +
-    'node [shape=plaintext];\n' +
-    'edge [weight=10];'
+function generateGraph (jsonPath) {
+  var json = require('./' + jsonPath);
 
-};
+  var edges = [];
 
-var printTail = function () {
-  return '}';
-};
+  var colors = {
+    'object': '#F78181',
+    'array': 'gold2',
+    'number': 'darkseagreen',
+    'string': 'beige'
+  };
 
-var printObjHead = function (name) {
-  var str = ' [label=<<table border="0" cellborder="1" cellpadding="2" cellspacing="0">\n'
-    + '\t<tr height="0"><td port="head" bgcolor="black"></td></tr>';
-  return name + str;
-};
+  var printHead = function () {
+    return 'digraph d {\n' +
+      'rankdir=LR\n' +
+        'nodesep=0.1\n' +
+        'concentrate=true\n' +
+        'splines=true\n' +
+        'node [fontname="Helvetica,Sans", fontsize=9, penwidth=1, margin="0, 0.04", height="0.2" ]\n' +
+        'node [shape=plaintext];\n' +
+        'edge [weight=10];'
+    };
 
-var printObjTail = function () {
-  return '\t</table>>]';
-};
+    var printTail = function () {
+      return '}';
+    };
 
-var printPort = function (name, value) {
-  var type = typeof value;
-  var bgcolor = colors[type];
+    var printObjHead = function (name) {
+      var str = ' [label=<<table border="0" cellborder="1" cellpadding="2" cellspacing="0">\n'
+        + '\t<tr height="0"><td port="head" bgcolor="black"></td></tr>';
+      return name + str;
+    };
 
-  var str = '\t<tr><td port="' + name + '" bgcolor="' + bgcolor + '">' + name + '</td></tr>';
-  return(str);
-};
+    var printObjTail = function () {
+      return '\t</table>>]';
+    };
 
-var printEdges = function () {
-  var str = '';
-  edges.forEach(
-    function (edge) {
-      str = str + edge.from + ' -> ' + edge.to + '\n';
-    });
-  return str;
-};
+    var printPort = function (name, value) {
+      var type = typeof value;
+      var bgcolor = colors[type];
 
-var print = function (str) {
-  console.log(str);
-};
+      var str = '\t<tr><td port="' + name + '" bgcolor="' + bgcolor + '">' + name + '</td></tr>';
+      return(str);
+    };
 
-var diagramize = function diagramize (object, name) {
+    var printEdges = function () {
+      var str = '';
+      edges.forEach(
+          function (edge) {
+            str = str + edge.from + ' -> ' + edge.to + '\n';
+          });
+      return str;
+    };
 
-  var keys;
+    var print = function (str) {
+      console.log(str);
+    };
 
-  if (object instanceof Array) {
-    if (object.length === 0) {
-      diagramize({'[ ]': '[ ]'}, name);
-      return;
-    }
+    var diagramize = function diagramize (object, name) {
 
-    diagramize(object[0], name);
-    return;
-  } else if (object instanceof Object) {
-    print(printObjHead(name));
-    keys = Object.keys(object);
-  } else {
-    return;
-  }
+      var keys;
 
-  keys.forEach(
-    function (key) {
-      var value = object[key];
-      print(printPort(key, value));
-    });
+      if (object instanceof Array) {
+        if (object.length === 0) {
+          diagramize({'[ ]': '[ ]'}, name);
+          return;
+        }
 
-  print(printObjTail());
+        diagramize(object[0], name);
+        return;
+      } else if (object instanceof Object) {
+        print(printObjHead(name));
+        keys = Object.keys(object);
+      } else if (object === null) {
+        diagramize({'null': 'null'}, name);
+        return;
+      } else  {
+        return;
+      }
 
-  keys.forEach(
-    function (key) {
-      var value = object[key];
-      if (typeof value === 'object') {
-        edges.push({from: name + ':' + key, to: key + ":head"});
-        diagramize(value, key);
-      };
-    });
+      keys.forEach(
+          function (key) {
+            var value = object[key];
+            print(printPort(key, value));
+          });
+
+      print(printObjTail());
+
+      keys.forEach(
+          function (key) {
+            var value = object[key];
+            if (typeof value === 'object') {
+              edges.push({from: name + ':' + key, to: key + ":head"});
+              diagramize(value, key);
+            };
+          });
 
 
-};
+    };
 
-print(printHead());
+    print(printHead());
 
-diagramize(json, 'root');
+    diagramize(json, 'root');
 
-print(printEdges());
+    print(printEdges());
 
-print(printTail());
+    print(printTail());
+  };
